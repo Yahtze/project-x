f0f1b0c1aeb7124e06d4783d12930f6ccc5a4354
Sam Stiles <samuel.dev.stiles@gmail.com>
2017-05-11 00:58:00 -0300

    Started v4 final (alpha)

---
+++ node+html5/v4 (final)/client.js
@@ -0,0 +1,78 @@
+/***************************************************************
+ * Initialization
+ ***************************************************************/
+
+var canvas = document.getElementById('game');
+var world;
+
+var inputs = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+    mouseX: 0,
+    mouseY: 0
+};
+
+canvas.addEventListener('mousemove', function(e) {
+    console.log('mouse moved', e.offsetX, e.offsetY);
+}, false);
+
+window.addEventListener('keydown', function (e){
+    if (e.keyCode === 87) inputs.up = true;
+    if (e.keyCode === 83) inputs.down = true;
+    if (e.keyCode === 65) inputs.left = true;
+    if (e.keyCode === 68) inputs.right = true;
+}, false);
+
+window.addEventListener('keyup', function (e){
+    if (e.keyCode === 87) inputs.up = false;
+    if (e.keyCode === 83) inputs.down = false;
+    if (e.keyCode === 65) inputs.left = false;
+    if (e.keyCode === 68) inputs.right = false;
+}, false);
+
+
+/***************************************************************
+ * Client/Server Communication
+ ***************************************************************/
+
+var socket = io(window.location.href);
+
+socket.on('world', function (updatedWorld) {
+    world = updatedWorld;
+});
+
+var lastBroadcastedInputs;
+
+function sendInputs () {
+    if (_.isEqual(inputs, lastBroadcastedInputs)) { return; }
+    socket.emit('inputs', inputs);
+    lastBroadcastedInputs = _.cloneDeep(inputs);
+}
+
+setInterval(sendInputs, 1000 / 30);
+
+/***************************************************************
+ * Rendering
+ ***************************************************************/
+
+ var draw = canvas.getContext('2d');
+
+ draw.imageSmoothingEnabled = false;
+ draw.translate(0.5, 0.5);
+
+function renderPlayer (player) {
+    draw.fillStyle = 'rgb(200,0,0)';
+    draw.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function render () {
+    draw.clearRect(0, 0, canvas.width, canvas.height);
+    if (world !== undefined) {
+        world.players.forEach(renderPlayer);
+    }
+    window.requestAnimationFrame(render);
+}
+
+render ();
---
+++ node+html5/v4 (final)/server.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var app = express().use(express.static(__dirname + '/'));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var _ = require('lodash');
+
+function getNowMs() { return new Date().getTime(); }
+function log(type, args) { console.log('['+type.toUpperCase()+'] ',args); }
+function debug(args) { log('debug', args); }
+function info(args) { log('info', args); }
+function warn(args) { log('warn', args); }
+function error(args) { log('error', args); }
+
+var world = {
+    height: 1000,
+    width: 1000,
+    players: [],
+    sockets: []
+};
+
+var defaultPlayer = {
+    x: 0,
+    y: 0,
+    width: 30,
+    height: 10,
+    inputs: {
+        up: false,
+        down: false,
+        left: false,
+        right: false
+    }
+};
+
+io.on('connection', function (socket){
+    info('A user connected!');
+    var thisPlayer = _.cloneDeep(defaultPlayer);
+    thisPlayer.id = socket.id;
+
+    world.players.push(thisPlayer);
+    world.sockets.push(socket);
+
+    socket.on('inputs', function (inputs) {
+        world.players.forEach(function(player) {
+            if (player.id === thisPlayer.id) {
+                player.inputs = inputs;
+            }
+        });
+    });
+
+    socket.on('disconnect', function () {
+        _.remove(world.players, function(player) {
+            return player.id === thisPlayer.id;
+        });
+    });
+});
+
+http.listen(3000, 'localhost', function () {
+    info('Game server started & awaiting connections.');
+});
+
+function updatePlayer (player) {
+    if (player.inputs.up) player.y -= 5;
+    if (player.inputs.down) player.y += 5;
+    if (player.inputs.left) player.x -= 5;
+    if (player.inputs.right) player.x += 5;
+}
+
+function tick () {
+    world.players.forEach(updatePlayer);
+}
+
+function broadcastState () {
+    var worldToBroadcast = {
+        players: world.players
+    };
+
+    world.sockets.forEach(function(socket) {
+        socket.emit('world', worldToBroadcast);
+    });
+}
+
+setInterval(tick, 1000 / 30);
+setInterval(broadcastState, 1000 / 30);
